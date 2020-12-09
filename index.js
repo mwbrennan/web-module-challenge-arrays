2fafc31fbdb9cc7004ee254cb9a86235807733cc
mwbrennan <mwb5@me.com>
2020-12-09 10:33:07 -0600

    task 2 updated

--- index.js
+++ index.js
@@ -64,8 +64,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(task_2){
+   if (task_2.length === 31) {
+       return true
+   } else {
+       return false
+   }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
